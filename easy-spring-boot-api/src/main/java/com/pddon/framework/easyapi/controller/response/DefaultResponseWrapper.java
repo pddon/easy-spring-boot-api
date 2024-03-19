28f4049a14c1b9023dfba7b1d36c3252f80df2ea
danyuandefine <zhangjun@danyuanblog.com>
2024-03-20 00:11:06 +0800

    api文档内响应外壳增加字段描述

--- easy-spring-boot-api/src/main/java/com/pddon/framework/easyapi/controller/response/DefaultResponseWrapper.java
+++ easy-spring-boot-api/src/main/java/com/pddon/framework/easyapi/controller/response/DefaultResponseWrapper.java
@@ -12,6 +12,8 @@ package com.pddon.framework.easyapi.controller.response;
 import java.io.Serializable;
 import java.util.Map;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -27,21 +29,44 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 @Setter
 @ToString(callSuper=true)
 @Accessors(chain=true)
+@ApiModel(value="DefaultResponseWrapper",description="响应信息外壳")
 public class DefaultResponseWrapper<T> implements Serializable {
 
 	/** 
 	 *serialVersionUID
 	 */
 	private static final long serialVersionUID = 1L;
+	@ApiModelProperty(
+			value="错误码，0成功，否则失败",
+			required = true
+	)
 	private Integer code;
+	@ApiModelProperty(
+			value="业务错误码"
+	)
 	private String subCode;
+	@ApiModelProperty(
+			value="错误提示信息，当接口失败时必填"
+	)
 	private String msg;
+	@ApiModelProperty(
+			value="接口响应数据信息"
+	)
 	private T data;
+	@ApiModelProperty(
+			value="响应数据数字签名"
+	)
 	private String sign;
+	@ApiModelProperty(
+			value="时间戳，用于数字签名和校验响应数据有效期"
+	)
 	private Long timestamp;
 	/**
 	 * 预留出来的可拓展系统参数字段
 	 */
+	@ApiModelProperty(
+			value="预留出来的可拓展系统参数字段"
+	)
 	private Map<String,Object> otherParams;
 	
 	
